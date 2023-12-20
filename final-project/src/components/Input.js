24644490956cafe8dbbfda91cb8e2e129f941fcf
Lauri Karvinen <laurikarvinen@icloud.com>
2023-12-20 11:39:37 +0200

    parannus ulkoasuun

--- final-project/src/components/Input.js
+++ final-project/src/components/Input.js
@@ -1,16 +1,24 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Context } from "../Context";
+import "../styles.css"; 
 
 export default function Input({ type, name, index }) {
   const [items, updateItem] = useContext(Context);
+  const [inputWidth, setInputWidth] = useState("1.75rem");
+
+  const handleInputChange = ({ target }) => {
+    updateItem(type, index, target.value);
+    setInputWidth(`${target.value.length + 1.75}rem`);
+  };
 
   return (
     <input
       type="text"
       inputmode="numeric"
       pattern="[0-9]*"
-      onChange={({ target }) => updateItem(type, index, target.value)}
+      onChange={handleInputChange}
       name={name.replace(" ", "-").toLowerCase()}
+      style={{ width: inputWidth, minWidth: "1.75rem" }}
     />
   );
 }
